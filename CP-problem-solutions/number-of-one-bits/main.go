7261221bb36cc40ee2adf02da4dcae6bb4371e45
Ifran Ruhin <ifrunruhin@gmail.com>
2025-05-16 20:54:13 +0600

    Add some problem solutions

---
+++ CP-problem-solutions/number-of-one-bits/main.go
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/number-of-1-bits/description/
+
+package main
+
+import (
+	"fmt"
+)
+
+func hammingWeight(n int) int {
+	bin := fmt.Sprintf("%b", n)
+	cnt := 0
+	for _, ch := range bin {
+		if ch == '1' {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func main() {
+	n := 2147483645
+	fmt.Println(hammingWeight(n))
+}
---
+++ CP-problem-solutions/valid-palindrom/main.go
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/valid-palindrome/description/
+
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	// Two-pointer approach after filtering
+	left, right := 0, len(s)-1
+
+	for left < right {
+		// Skip non-alphanumeric on the left
+		for left < right && !unicode.IsLetter(rune(s[left])) && !unicode.IsDigit(rune(s[left])) {
+			left++
+		}
+		// Skip non-alphanumeric on the right
+		for left < right && !unicode.IsLetter(rune(s[right])) && !unicode.IsDigit(rune(s[right])) {
+			right--
+		}
+
+		// Compare lowercased letters
+		if unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])) {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}
+
+func main() {
+	s := "A man, a plan, a canal: Panama"
+	fmt.Println(isPalindrome(s)) // Output: true
+}
+
