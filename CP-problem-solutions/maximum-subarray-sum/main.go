7e8c42d6ba7a4ac891752ee6f414e0a1a3969542
Ifran Ruhin <ifrunruhin@gmail.com>
2025-05-17 18:24:43 +0600

    Add some problem solutions

---
+++ CP-problem-solutions/maximum-subarray-sum/main.go
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/maximum-subarray/description/
+
+package main
+
+import "fmt"
+
+func maxOfTwo(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxSubArray(nums []int) int {
+	maxSub := nums[0]
+	curSum := 0
+
+	for _, v := range nums {
+		if curSum < 0 {
+			curSum = 0
+		}
+		curSum += v
+		maxSub = maxOfTwo(curSum, maxSub)
+	}
+	return maxSub
+}
+
+func main() {
+	slc := []int{-2,1,-3,4,-1,2,1,-5,4}
+	fmt.Println(maxSubArray(slc))
+}
