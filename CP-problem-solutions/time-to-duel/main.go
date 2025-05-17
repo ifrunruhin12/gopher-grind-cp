bec433a9861376cc2fb5469a4dd3ac62acda5aae
Ifran Ruhin <ifrunruhin@gmail.com>
2025-05-18 00:20:15 +0600

    Add some problem solutions

---
+++ CP-problem-solutions/time-to-duel/main.go
@@ -0,0 +1,63 @@
+//https://codeforces.com/contest/2109/problem/A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	in  = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func readLine() string {
+	line, _ := in.ReadString('\n')
+	return strings.TrimSpace(line)
+}
+
+func readInt() int {
+	n, _ := strconv.Atoi(readLine())
+	return n
+}
+
+func readInts() []int {
+	line := readLine()
+	parts := strings.Fields(line)
+	nums := make([]int, len(parts))
+	for i, s := range parts {
+		nums[i], _ = strconv.Atoi(s)
+	}
+	return nums
+}
+
+func main() {
+	defer out.Flush()
+	t := readInt()
+	for i := 0; i < t; i++ {
+		n := readInt()
+		arr := readInts()
+
+		allOnes := true
+		hasConsecutiveZeros := false
+
+		for j := 0; j < n; j++ {
+			if arr[j] == 0 {
+				allOnes = false
+			}
+			if j > 0 && arr[j] == 0 && arr[j-1] == 0 {
+				hasConsecutiveZeros = true
+			}
+		}
+
+		if allOnes || hasConsecutiveZeros {
+			fmt.Fprintln(out, "YES")
+		} else {
+			fmt.Fprintln(out, "NO")
+		}
+	}
+}
