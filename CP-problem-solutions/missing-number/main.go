998126f3668ada033e9b08c1015d70d80bb5e5cd
Ifran Ruhin <ifrunruhin@gmail.com>
2025-05-24 23:57:22 +0600

    Add some problem solutions

---
+++ CP-problem-solutions/missing-number/main.go
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/missing-number/
+
+package main
+
+import (
+	"fmt"
+)
+
+func sum(nums []int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
+func missingNumber(nums []int) int {
+	total := len(nums) * (len(nums) + 1) / 2
+	return total - sum(nums)
+}
+
+func main() {
+	fmt.Println(missingNumber([]int{3, 0, 1}))
+	fmt.Println(missingNumber([]int{0, 1}))
+	fmt.Println(missingNumber([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
+}
