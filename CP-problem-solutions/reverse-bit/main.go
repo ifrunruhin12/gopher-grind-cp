4f62b0022268bb3fb1652de97cffb506fbeeac48
Ifran Ruhin <ifrunruhin@gmail.com>
2025-05-30 23:59:33 +0600

    Add some problem solutions

---
+++ CP-problem-solutions/reverse-bit/main.go
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/reverse-bits/description/
+
+package main
+
+import (
+	"fmt"
+)
+
+func reverseBits(num uint32) uint32 {
+	var result uint32 = 0
+	for i := 0; i < 32; i++ {
+		result <<= 1
+		result |= num & 1
+		num >>= 1
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(reverseBits(43261596))     // Output: 964176192
+	fmt.Println(reverseBits(4294967293))   // Output: 3221225471
+}
+
