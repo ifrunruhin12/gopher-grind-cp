d5afac07977acc9856e2bea5a455e43b7105cb24
Ifran Ruhin <ifrunruhin@gmail.com>
2025-07-01 20:49:22 +0600

    Updated the whole structure of the project and refactor

--- CP-problem-solutions/bit-manipulation/reverse-bit/main.go
+++ CP-problem-solutions/bit-manipulation/reverse-bit/main.go
@@ -1,5 +1,5 @@
 //https://leetcode.com/problems/reverse-bits/description/
-//
+//Bit Manipulation
 
 package main
 
--- CP-problem-solutions/sorting/another-monster-problem/main.go
+++ CP-problem-solutions/sorting/another-monster-problem/main.go
@@ -1,4 +1,5 @@
 //https://www.codechef.com/problems/YETMON
+//sorting
 
 package main
 
